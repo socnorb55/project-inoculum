0e611f532711977e017e410fc25c5bd01a07a220
Keelan Zeigler <socnorb55@gmail.com>
2025-11-19 21:52:12 -0500

    feat: adding dough and dough status objects

--- src/database/models.rs
+++ src/database/models.rs
@@ -9,3 +9,27 @@ pub struct StarterFeeding {
     pub water_amount: f32,
     pub water_temp: f32,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Dough {
+    pub fat: Option<f32>,
+    pub flour: f32,
+    pub hydration: f32,
+    pub leaven: f32,
+    pub name: String,
+    pub salt: f32,
+    pub status: DoughStatus,
+    pub sugar: Option<f32>,
+    pub timestamp: Datetime,
+    pub total_weight: f32,
+    pub water: f32,
+}
+
+#[derive(Debug, serde::Serialize, serde::Deserialize)]
+pub enum DoughStatus {
+    BulkProofing,
+    Cooking,
+    Created,
+    SecondaryProofing,
+    Shapping,
+}
