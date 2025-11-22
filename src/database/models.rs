de7c2114000289e6ac342a358ddde90a5d710510
Keelan Zeigler <socnorb55@gmail.com>
2025-11-22 10:12:28 -0500

    fix: updating timestamp fields

--- src/database/models.rs
+++ src/database/models.rs
@@ -4,7 +4,6 @@ use surrealdb::sql::Datetime;
 #[derive(Debug, Serialize, Deserialize)]
 pub struct StarterFeeding {
     pub flour_amount: f32,
-    pub id: String,
     pub starter_amount: f32,
     pub timestamp: Datetime,
     pub water_amount: f32,
@@ -16,14 +15,14 @@ pub struct Dough {
     pub fat: Option<f32>,
     pub flour: f32,
     pub hydration: f32,
-    pub id: String,
     pub leaven: f32,
     pub name: String,
     pub salt: f32,
+    pub start_timestamp: Datetime,
     pub status: DoughStatus,
     pub sugar: Option<f32>,
-    pub timestamp: Datetime,
     pub total_weight: f32,
+    pub update_timestamp: Datetime,
     pub water: f32,
 }
 
